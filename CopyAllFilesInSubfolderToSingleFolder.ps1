269260d9d0033bc6b90d0e9de5aa0b6bcb1d54a8
luisramirezmex <luis.ramirez.mex@gmail.com>
2017-07-20 16:55:00 -0500

    Update and rename CopyAllFilesInSubfolderToSingleFolder to CopyAllFilesInSubfolderToSingleFolder.ps1

--- CopyAllFilesInSubfolderToSingleFolder.ps1
+++ CopyAllFilesInSubfolderToSingleFolder.ps1
@@ -1,3 +1,4 @@
+#Code found at: https://stackoverflow.com/questions/17497667/how-to-copy-certain-files-w-o-folder-hierarchy-but-do-not-overwrite-existing
 $srcdir = "C:\source\";
 $destdir = "C:\destination\";
 $files = (Get-ChildItem $srcdir -recurse -filter *.sql | where-object {-not ($_.PSIsContainer)});
